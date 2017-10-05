a7caf2f98334915044de2f744ea226d9ab873348
briavers <brian.verschoore@student.arteveldehs.be>
2017-10-05 09:41:38 +0200

    first commit

---
+++ oef1.1/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>index.php oef 1.1</title>
+</head>
+<body>
+<?php
+    // Hier komt PHP-code tussen de PHP-begintag en de PHP-eindtag.
+    $naam = 'Brian Verschoore';
+
+    echo('welkom' .$naam);
+    
+?>  
+</body>
+</html>
---
+++ oef1.2/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>index.php oef 1.2</title>
+</head>
+<body>
+<?php
+    // Hier komt PHP-code tussen de PHP-begintag en de PHP-eindtag.
+    $string1 = 'hello';
+    $string2 = 'world';
+
+    echo($string1 .$string2);
+    
+?>  
+</body>
+</html>
